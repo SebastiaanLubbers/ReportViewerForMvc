c05fcefb5e24aecca4917e98af6706cac333f1be
Sebastiaan Lubbers <github@duikmeester.nl>
2022-06-13 13:47:03 +0200

    Update NuGet packages

--- ReportViewerForMvc.Example/App_Start/BundleConfig.cs
+++ ReportViewerForMvc.Example/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace ReportViewerForMvc.Example
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.slim.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -18,10 +18,12 @@ namespace ReportViewerForMvc.Example
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/respond.matchmedia.addListener.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
